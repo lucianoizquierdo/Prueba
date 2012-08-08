f3ac2694b758ce47e01b63a889bc80cadce806bf
lucianoizquierdo <luciano.e.izquierdo@hotmail.com>
2012-08-08 17:28:53 -0300

    08/07/2012 Agrego una linea

--- src/com/luciano/Prueba.java
+++ src/com/luciano/Prueba.java
@@ -8,6 +8,7 @@ public class Prueba {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola Mundo");
+		System.out.println("jimi hendrix experience!");
 
 	}
 
